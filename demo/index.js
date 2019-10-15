d3ac648895426edd5c169bce2d20eb33dda9d76a
xhfhttp@163.com <xfhhttp@163.com>
2019-10-15 22:36:21 +0800

    a

--- demo/index.js
+++ demo/index.js
@@ -1,3 +1,10 @@
+/*
+    js开发中的问题
+        1.使用了不存在的变量、函数或成员
+        2.把一个不确定类型，当做一个缺的定类型进行处理
+        3.使用null和undefined
+    js的原罪
+*/
 function getUserName() {
     if (Math.random() < 0.5) {
         return 'yuan jin';
